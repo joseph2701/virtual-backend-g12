f8f2b0c768d5b2f76a3787910bfa322254fb8c92
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-02-28 23:04:26 -0500

    fin dia01 - semana01

---
+++ Dia 1/04-operadores-asignacion.py
@@ -0,0 +1,29 @@
+#operadores de asignacion para
+numero1,numero2=10,20 
+# igual =
+print(numero1=10)
+#decremento
+numero1 +=1
+print(numero1)
+# #decremento
+numero1 -=1
+print(numero1)
+
+#multiplicador
+numero1 *=2
+
+print(numero1)
+
+#dividendo
+numero1 /=2
+print(numero1)
+
+#potencia
+numero1 **=2
+print(numero1)
+
+#en caso de los strings, solo se pueden
+#sumar y multiplicador
+nombre='joseph'
+ciudad='mendoza'
+print (nombre+ciudad)
