b35d1554a71da31de527c1dcb99d21db4293f102
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-04 22:42:23 -0500

    django avance

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Etiqueta(models.Model):
+    #    https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-types
+    #   https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-options
+    id=models.AutoField(primary_key=True,unique=True,null=False)
+    nombre=models.CharField(max_length=20,unique=True,null=False)
+    #Columnas de auditoria
+    #son columnas que ayudan al seguimiento de cracion de registros
+    #fecha en la que se creo el registro
+    createdAt =models.DateTimeField(auto_now_add=True,db_column='created_at')
+    #fecha en la que se modifico el registro
+    updateAt=models.DateTimeField(auto_now=True,db_column='updated_at')
+
+
+class Metea:
+    #cambia el nombre de la tabla en la db ()
+    db_table='etiquetas'
+    # osea ASC del nombre, solo para GET mediante ORM
+    ordering=['-nombre']
