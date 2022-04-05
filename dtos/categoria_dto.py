dae1f7b16ae130eafa7411b2e8c71c4e6a77f373
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-04 22:22:46 -0500

    django

--- dtos/categoria_dto.py
+++
@@ -1,6 +0,0 @@
-from config import validador
-from models.categorias import Categoria
-
-class CategoriaResponseDTO(validador.SQLAlchemyAutoSchema):
-    model=Categoria
-    
--- dtos/change_password_dto.py
+++
@@ -1,6 +0,0 @@
-from config import validador,fields,validate
-
-class ChangePasswordRequestDTO(validador.Schema):
-    token=fields.String(required=True)
-    password=fields.String(validate=validate.Length(min=6))
-
