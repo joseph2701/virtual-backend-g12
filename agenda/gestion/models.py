4931079a707f6923d7a5174abff25358a796b901
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-05 23:03:18 -0500

    s05 dia2

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -12,10 +12,56 @@ class Etiqueta(models.Model):
     createdAt =models.DateTimeField(auto_now_add=True,db_column='created_at')
     #fecha en la que se modifico el registro
     updateAt=models.DateTimeField(auto_now=True,db_column='updated_at')
+    class Meta:
+        #cambia el nombre de la tabla en la db ()
+        db_table='etiquetas'
+        # osea ASC del nombre, solo para GET mediante ORM
+        ordering=['-nombre']
 
 
-class Metea:
-    #cambia el nombre de la tabla en la db ()
-    db_table='etiquetas'
-    # osea ASC del nombre, solo para GET mediante ORM
-    ordering=['-nombre']
+class Tareas(models.Model):
+    #    https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-types
+    #   https://docs.djangoproject.com/en/4.0/ref/models/fields/#field-options
+    class CategoriasOpciones(models.TextChoices):
+        TODO='TODO','TO_DO'
+        IN_PROGRESS='IP','IN_PROGRESS'
+        DONE='DONE','DONE'
+        CANCELLED='CANCELLED','CANCELLED'
+
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=20,unique=True,null=False)
+    categorias=models.CharField(max_length=45,choices=CategoriasOpciones.choices,default=CategoriasOpciones.TODO)
+
+    #OTRA FORMA USANDO TUPLAS
+    # categorias=models.CharField(max_length=45,choices=[
+    #   ('TODO','TO_DO'),
+    #   ('IP','IN_PROGRESS'),
+    #   ('DONE','DONE'),
+    #   ('CANCELLED','CANCELLED')
+    # ],default='TODO')
+
+    fechaCaducidad=models.DateTimeField(db_column='fecha_caducidad')
+    importancia=models.IntegerField(null=False)
+    descripcion=models.TextField()
+    #Columnas de auditoria
+    #son columnas que ayudan al seguimiento de cracion de registros
+    #fecha en la que se creo el registro
+    createdAt =models.DateTimeField(auto_now_add=True,db_column='created_at')
+    #fecha en la que se modifico el registro
+    updateAt=models.DateTimeField(auto_now=True,db_column='updated_at')
+    
+    #solo se usa esta forma, cuando la tabla es de muchos a muchos y solo us foreign keys
+    etiquetas=models.ManyToManyField(to=Etiqueta,related_name='tareas')
+
+    class Meta:
+        #cambia el nombre de la tabla en la db ()
+        db_table='tareas'
+        # osea ASC del nombre, solo para GET mediante ORM
+        #ordering=['-nombre']
+
+#si la tabla tiene campos aparte
+#   class TareasEtiquetas(models.Model):
+#   EtiquetaFK=models.ForeignKey(To=Etiqueta)
+#   TareaFK=models.ForeignKey(To=Tareas)
+#   las demas columnas
+
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+@api_view(http_method_names=['GET','POST'])
+def inicio(request:Request):
+    #request sera toda la informacion enviada por el cliente
+    print(request.method)
+    print(request)
+    #comportamiento GET
+    if request.method=='GET':
+        return Response(data={
+        'message':'Bienvenido a mi API de agenda'
+        })
+        #comportamiento POST
+    elif request.method=='POST':
+        return Response(data={
+        'message':'Hiciste un post'
+        })
+
+class PruebaApiView(ListAPIView):
+    serializer_class = None
