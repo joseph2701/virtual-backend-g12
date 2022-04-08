58785e7c0fec64e034f432c44f562547dd23b7d8
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-07 21:34:02 -0500

    creacion de Tareas

--- agenda/gestion/serializers.py
+++ agenda/gestion/serializers.py
@@ -7,17 +7,44 @@ class PruebaSerializer(serializers.Serializer):
     dni=serializers.RegexField(max_length=8,min_length=8,regex="[0-9]")
     #dni=serializers.IntegerField(min_value=10000000,max_value=99999999)
 
-class TareaSerializer(serializers.ModelSerializer)    :
-
+class TareasSerializer(serializers.ModelSerializer)    :
     class Meta:
         model=Tareas 
         fields='__all__'
+        extra_kwargs={
+            'etiquetas':{
+                'write_only':True
+            }
+        }
         #o se usa fields, o se usa exclude
         # indica que columnas no quiero utilizar
         #exclude=['importancia']
+       # depth=1
+        #la profundidad max es 10
 
-class EtiquetaSerializer(serializers.ModelSerializer)    :
+class TareaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Tareas
+        fields='__all__'
+        depth=1
 
+class EtiquetaSerializer(serializers.ModelSerializer):
+    #se usa para acceder al related name llamado en el otro lado
+    #readonly es para indicarle solo para GET
+
+    tareas=TareaSerializer(many=True,read_only=True)
+    #otra forma:
+    #no_es_tareas=TareaSerializer(many=True,read_only=True,Source=True)
+    
     class Meta:
         model=Etiqueta
         fields='__all__'
+        #aqui se puede colocar los campos os cuales n se van a utilizar 
+        #en una de las vistas
+        extra_kwargs={
+            'nombre':{'write_only':True},
+            'id':{'read_only':True},
+        }
+        #asigna los campos que serean solo lectura
+        read_only_fields=['createAt']
+        # write_only_fields=['createAt']   NO EXISTEEEE
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -2,8 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.generics import ListAPIView,ListCreateAPIView
-from .serializers import PruebaSerializer,TareaSerializer,EtiquetaSerializer 
+from .serializers import PruebaSerializer,TareasSerializer,EtiquetaSerializer,TareaSerializer 
 from .models import Tareas ,Etiqueta
+from rest_framework import status
+#from datetime import datetime
+from django.utils import timezone
 
 @api_view(http_method_names=['GET','POST'])
 def inicio(request:Request):
@@ -52,7 +55,47 @@ class PruebaApiView(ListAPIView):
 class TareasApiView(ListCreateAPIView):
     #select * from Tareas
     queryset=Tareas.objects.all()  
-    serializer_class=TareaSerializer
+    serializer_class=TareasSerializer
+
+    def post(self,request):
+        serializador=self.serializer_class(data=request.data)
+        if serializador.is_valid():
+            #luego de ejecutar is_valid, se pueden usar::: retornan diccionarios
+            #serializador.initial_data es data inicial sin validacion
+            #serializador.validated_data es data ya validada
+            #print(type( serializador.validated_Data.get('fechaCaducidad')))
+            print( serializador.validated_data.get('fechaCaducidad'))
+            fechaCaducidad=serializador.validated_data.get('fechaCaducidad')            
+            importancia=serializador.validated_data.get('importancia')     
+            #if datetime.now(timezone.utc)>fechaCaducidad:
+            #otra forma
+            if importancia<0 or importancia>10:
+                return Response(
+                    data={
+                        'message':'importancia no esta entre 0 - 10'
+                    },status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            if timezone.now()>fechaCaducidad:
+                return Response(
+                    data={
+                        'message':'Fecha no puede ser menor que la fecha actual'
+                    },status=status.HTTP_400_BAD_REQUEST
+                )
+            else:
+                return Response(data='Registrado con exito',status=status.HTTP_201_CREATED)
+        else:
+            #muestra los errores
+            #   serializador.errors
+            return Response(
+                data={
+                    'mensaje':'La data no es valida',
+                    'content':serializador.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+
 
 class EtiquetasApiView(ListCreateAPIView):
     #select * from Tareas
