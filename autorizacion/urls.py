d6e8df0747e4fcdbb14a26cf914cdde14ef9b2aa
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-25 22:35:05 -0500

    limpieza rama

--- autorizacion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegistroUsuarioApiView
-#me retrnara dos toeken de acceso y de refresh
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-    path('registro/', RegistroUsuarioApiView.as_view()),
-    path('inicio-sesion/',TokenObtainPairView.as_view())
-]
