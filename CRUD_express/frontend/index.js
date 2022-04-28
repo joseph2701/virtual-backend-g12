3a5192a616ce0b9a5ec61d961a75745439ca3392
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-04-27 21:44:36 -0500

    creacion proyecto minimarket

---
+++ CRUD_express/frontend/index.js
@@ -0,0 +1,8 @@
+const BASE_URL='http://localhost:3000'
+
+async function pedirProductos(){
+    const respuesta=await fetch(`${BASE_URL}/productos`,{method:"GET"});
+    const data= await respuesta.json();console.log(data);
+}
+
+pedirProductos();
