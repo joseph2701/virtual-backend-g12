1f3aabf5e1620057795cf9992f2755a3ef74a7bc
Joseph18122701 <joseph.mendoza.gonzales@gmail.com>
2022-05-04 22:46:33 -0500

    clean-brunch

--- minimarket/src/routes/usuarios.routes.js
+++
@@ -1,6 +0,0 @@
-import {Router} from "express";
-import {crearUsuario,login} from "../controllers/usuarios.controller.js";
-
-export const usuariosRouter=Router();
-usuariosRouter.route("/Registro").post(crearUsuario);
-usuariosRouter.route("/login").post(login);
